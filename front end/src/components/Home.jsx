7427146ee868d54752a06eddea855ab8a86ef92a
Adam Hussain <adamh93@verizon.net>
2020-07-09 09:48:37 -0400

    done: Refactor App.js to use Home component for default route, Remove other components in App.js and place them in Home component

---
+++ front end/src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import LatestNews from './LatestNews';
+import KnowledgeSection from './KnowledgeSection';
+
+
+
+const Home = () => {
+
+  return (
+    <div>
+      <LatestNews />
+      <KnowledgeSection />
+    </div>
+  );
+}
+
+export default Home;
